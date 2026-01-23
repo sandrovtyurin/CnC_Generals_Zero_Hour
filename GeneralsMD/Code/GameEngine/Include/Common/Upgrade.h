da940155ce3872e5e6c65547cf0991cfea127b58
sandrovtyurin <sandro.vtyurin@gmail.com>
2026-01-23 12:42:15 +0300

    Update Upgrade.h

--- GeneralsMD/Code/GameEngine/Include/Common/Upgrade.h
+++ GeneralsMD/Code/GameEngine/Include/Common/Upgrade.h
@@ -54,7 +54,7 @@ enum UpgradeStatusType
 };
 
 //The maximum number of upgrades. 
-#define UPGRADE_MAX_COUNT 128
+#define UPGRADE_MAX_COUNT 150
 
 typedef BitFlags<UPGRADE_MAX_COUNT>	UpgradeMaskType;
 
